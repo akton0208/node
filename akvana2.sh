13aef932a574216613ae0245df9ce7f5be70ba7e
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-10-25 20:28:37 +0800

    Update akvana2.sh

--- akvana2.sh
+++ akvana2.sh
@@ -298,14 +298,7 @@ delete_all_files() {
 }
 
 # 一鍵執行所有步驟的函數
-run_all_steps() {
-    #install_files
-    #create_wallet
-    #export_coldkey
-    #export_hotkey
-    #deploy_smart_contracts
-    #setup_validator
-}
+
 
 # 主程序
 while true; do
