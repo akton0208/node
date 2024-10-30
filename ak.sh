51ab525d46ce8deb05d873759710e78e153ef426
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-10-31 01:16:38 +0800

    Create ak.sh

---
+++ ak.sh
@@ -0,0 +1,15 @@
+echo -e "\e[1;32m
+
+
+
+      A           K     K
+     A A          K   K
+    A   A         K K
+   AAAAAAA        K  K
+  A       A       K    K
+ A         A      K      K
+
+";
+echo -e "\e[0m"
+echo -e ""
+echo -e ""
